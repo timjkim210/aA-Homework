a52896f346ddc25d2a2f6b57f154c3aaafa2f87e
timjkim210 <timjkim210@gmail.com>
2020-12-03 22:56:25 -0800

    define instance methods for stack.rb

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,17 +1,17 @@
 class Stack
-    def initialize
-      # create ivar to store stack here!
+    def initialize(height, value)
+        @stack = Array.new(height) {value}
     end
 
     def push(el)
-      # adds an element to the stack
+        @stack.push(el)
     end
 
     def pop
-      # removes one element from the stack
+        @stack.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+      @stack[-1]
     end
-  end
+end
